d40e811d7340d5c69895fc376467b62324f5b587
yo-ismaili <ismailiyounes20@gmail.com>
2021-11-26 19:05:59 +0100

    libft-42

---
+++ ft_split.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yismaili <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/09 11:46:30 by yismaili          #+#    #+#             */
+/*   Updated: 2021/11/26 19:04:45 by yismaili         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_lenwrd(const char *s, char c)
+{
+	int	len;
+
+	len = 0;
+	while (*s != '\0')
+	{
+		if (*s != c)
+		{
+			len++;
+			while (*s != '\0' && *s != c)
+			{
+				s++;
+			}
+			if (*s == '\0')
+				return (len);
+		}
+		s++;
+	}
+	return (len);
+}
+
+static void	*ft_free(char **dest, int j)
+{
+	int i;
+
+	i = 0;
+	while (i < j)
+	{
+		free(dest[j]);
+		i++;
+	}
+	free (dest);
+	return (NULL);
+}
+
+static char	**ft_checkalocc(char **dest, const char *s, char c)
+{
+	size_t	i;
+	int		j;
+	size_t	index;
+
+	i = 0;
+	j = 0;
+	while (i <= ft_strlen(s))
+	{
+		while (s[i] != '\0' && s[i] == c)
+			i++;
+		index = i;
+		while (s[i] != '\0' && s[i] != c)
+			i++;
+		if (index != i)
+		{
+			dest[j] = ft_substr(s, index, i - index);
+			if (!dest[j])
+				return (ft_free(dest, j));
+			j++;
+		}
+		i++;
+	}
+	dest[j] = 0;
+	return (dest);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**dest;
+
+	if (!s)
+		return (NULL);
+	dest = (char **)malloc((ft_lenwrd(s, c) + 1) * sizeof(char *));
+	if (!dest)
+		return (NULL);
+	dest = ft_checkalocc(dest, s, c);
+	return (dest);
+}
+#include<stdio.h>
+int main (void)
+{ char str[] = "-----youne--sss---khi--fhfhj-llfkfl-pk,x---";
+char c = '-';
+char **str1 = ft_split(str, c);
+int i = 0;
+while (str1 != NULL && str1[i] != NULL)
+{
+	printf("%s\n",str1[i]);
+	i++;
+}
+}
+
---
+++ ft_strjoin.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yismaili <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/07 20:01:52 by yismaili          #+#    #+#             */
+/*   Updated: 2021/11/07 21:07:26 by yismaili         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		i;
+	int		j;
+	char	*dst;
+
+	i = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	dst = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!dst)
+		return (NULL);
+	while (s1[i])
+	{
+		dst[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		dst[i] = s2[j];
+		j++;
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
